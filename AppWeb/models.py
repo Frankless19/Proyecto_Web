2053755520b58b79fde0477a4cc493abea42cce0
IbaiAbad <ibai.abad@opendeusto.es>
2023-04-13 19:15:20 +0200

    Ticket Class without multiple atributes

--- AppWeb/models.py
+++ AppWeb/models.py
@@ -16,3 +16,21 @@ class Equipo(models.Model):
     def __str__(self):
         return f" Equipo: {self.marca} {self.modelo}. Numero de serie: {self.nserie}"
     
+
+#Creacion de la clase Ticket
+class Ticket(models.Model):
+    nref = models.CharField.primary_key(max_length = 20)
+    Titulo = models.CharField(max_length = 10)
+    Descripcion = models.CharField(max_length = 50)
+    FechaApertura = models.DateTimeField()
+    FechaResolucion = models.DateTimeField()
+    NivelUrgencia = models.(max_length = 5)
+    TipoTicket = models.(max_length = 15)
+    EstadoTicket = models.BooleanField()
+    Empleadoticket = models.CharField(max_length = 15)
+    Cometarios = models.CharField(max_length = 40)
+    
+    #Devuelve un string con la marca, el modelo, y el numero de serie de cada Equipo creado.
+    def __str__(self):
+        return f" Referencia: {self.nref}. Titulo: {self.Titulo}. Descripcion: {self.Descripcion}"
+    
