139958099db1e77443dd3e09010cbadcd131111e
Frankless19 <david.franco@opendeusto.es>
2023-04-13 18:58:21 +0200

    modelo equipo
    models equipo

--- AppWeb/models.py
+++ AppWeb/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+#Creacion de la clase equipo
+class Equipo(models.Model):
+    nserie = models.CharField.primary_key(max_length = 20)
+    modelo = models.CharField(max_length = 50)
+    marca = models.CharField(max_length = 30)
+    tipoEquipo = models.CharField(max_length = 20)
+    fechaAdquisicion = models.DateTimeField()
+    fechaPuestaMarcha = models.DateTimeField()    
+    provNombre = models.CharField(max_length = 30)
+    provTelefono = models.IntegerField(default= 0)
+    planta = models.CharField(max_length = 15)
+    
+    #Devuelve un string con la marca, el modelo, y el numero de serie de cada Equipo creado.
+    def __str__(self):
+        return f" Equipo: {self.marca} {self.modelo}. Numero de serie: {self.nserie}"
+    
