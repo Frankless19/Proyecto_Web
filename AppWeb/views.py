2921a7fed5553c3f7ff6a2c0f3733ab542b9b44f
Frankless19 <david.franco@opendeusto.es>
2023-04-19 19:19:28 +0200

    fallos

--- AppWeb/views.py
+++ AppWeb/views.py
@@ -4,22 +4,16 @@ from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Empleado, Equipo, Ticket
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Bienvenido")
-
 class ListaEmpleado(CreateView, ListView):
     model = Empleado
     template_name = 'listadoEmpleados.html'
-    success_url = 'http://127.0.0.1:8000/AppWeb/listadoEmpleados/'
     
 class ListaEquipo(CreateView, ListView):
     model = Equipo
     template_name = 'listadoEquipos.html'
-    success_url = 'http://127.0.0.1:8000/AppWeb/listadoEquipos/'
     
 class ListaTicket(CreateView, ListView):
     model = Ticket
     template_name = 'listadoTickets.html'
-    success_url = 'http://127.0.0.1:8000/appWeb/listadoTickets/'
     
     
