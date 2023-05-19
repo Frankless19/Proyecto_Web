e4c914488feb098d5e207cbb06f3f4c39ad68434
IbaiAbad <ibai.abad@opendeusto.es>
2023-05-20 00:46:25 +0200

    Funcionalidad Ibai terminada

--- AppWeb/static/JS/mostrarmas.js
+++ AppWeb/static/JS/mostrarmas.js
@@ -1,13 +1,31 @@
 window.addEventListener('load', init, false);
 function init() {
-    let div = document.querySelector('#ocultar-y-mostrar');
-    div.style.visibility = 'visible';
-    let boton = document.querySelector('#ocultar-mostrar');
-    boton.addEventListener('click', function (e) {
-        if(div.style.visibility === 'visible'){
-            div.style.visibility = 'hidden';
-        }else{
-            div.style.visibility = 'visible';
+    let div = document.querySelector('.ocultar-y-mostrar');
+    div.style.display = 'block';
+    let mostrar = document.querySelector('.ocultar-mostrar1');
+    mostrar.style.display = 'none';
+    let ocultar = document.querySelector('.ocultar-mostrar2');
+    mostrar.addEventListener('click', function (e) {
+        if (div.style.display === 'block') {
+            ocultar.style.display = 'block';
+            mostrar.style.display = 'none';
+
+        } else {
+            div.style.display = 'block';
+            mostrar.style.display = 'none';
+            ocultar.style.display = 'block';
+        }
+    }, false);
+
+    ocultar.addEventListener('click', function (e) {
+        if (div.style.display === 'none') {
+            mostrar.style.display = 'none';
+            ocultar.style.display = 'block';
+        } else {
+            div.style.display = 'none';
+            mostrar.style.display = 'block';
+            ocultar.style.display = 'none';
         }
     }, false);
 }
+
