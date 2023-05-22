38c9a784af89b8897e845e8facc917a00e012e82
Frankless19 <david.franco@opendeusto.es>
2023-05-22 16:37:45 +0200

    js validarForm (pruebas)

--- AppWeb/static/JS/validarForm.js
+++ AppWeb/static/JS/validarForm.js
@@ -1,20 +1,39 @@
-function validarForm() {
-  const numeroserie = document.getElementById('id_nserie').value;
+// Validación de formulario de Equipo
+var formEquipo = document.getElementById('miFormulario');
 
-  if (numeroserie === "" || numeroserie === null || numeroserie === "None") {
-    alert('El campo numero de serie debe rellenarse');
-    return false;
+formEquipo.addEventListener('submit', function(event) {
+  var provTelefonoInput = document.getElementById('id_provTelefono');
+  var provTelefono = provTelefonoInput.value.trim();
+  
+  if (!/^[\d]+$/.test(provTelefono)) {
+    event.preventDefault();
+    alert('El teléfono del proveedor solo puede contener números.');
   }
+});
 
-  return true;
+// Validación de formulario de Ticket
+var formTicket = document.getElementById('miFormulario');
 
-  const marca = document.getElementById('id_marca').value;
-
-  if (marca === "" || marca === null || marca === "None") {
-    alert('El campo numero de serie debe rellenarse');
-    return false;
+formTicket.addEventListener('submit', function(event) {
+  var empleadoDniInput = document.getElementById('id_empleado-dni');
+  var empleadoDni = empleadoDniInput.value.trim();
+  
+  if (!/^[0-9]{8}[A-Z]$/.test(empleadoDni)) {
+    event.preventDefault();
+    alert('El DNI del empleado debe contener 8 números seguidos de una letra mayúscula.');
   }
+});
 
-  return true;
-}
-document.getElementById('miFormulario').addEventListener('submit', validarForm);
+// Validación de formulario de Empleado
+var formEmpleado = document.getElementById('miFormulario');
+
+formEmpleado.addEventListener('submit', function(event) {
+  var emailInput = document.getElementById('id_email');
+  var email = emailInput.value.trim();
+  
+  if (!/@/.test(email)) {
+    event.preventDefault();
+    alert('El campo de correo electrónico debe contener un símbolo "@"');
+  }
+});
+
